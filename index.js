61f56f3711602b157369e54e818d490f391c0482
benjosephjones07 <benjosephjones07@gmail.com>
2022-01-31 10:30:21 +0000

    follow dev ed tutorial up to remove point click events

--- index.js
+++ index.js
@@ -1,63 +1,88 @@
-const cards = document.querySelectorAll('.memory-card');
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+const playerLives = 6;
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+playerLivesCount.textContent = playerLives;
 
-function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+const getData = () => [
+    {imgSrc: 'assets/images/beatles.jpeg', name: 'beatles'},
+    {imgSrc: 'assets/images/blink182.jpeg', name: 'blink 182'},
+    {imgSrc: 'assets/images/fkatwigs.jpeg', name: 'fka twigs'},
+    {imgSrc: 'assets/images/fleetwood.jpeg', name: 'fleetwood'},
+    {imgSrc: 'assets/images/joy-division.jpeg', name: 'joy division'},
+    {imgSrc: 'assets/images/ledzep.jpeg', name: 'led zeppelin'},
+    {imgSrc: 'assets/images/metallica.jpeg', name: 'metallica'},
+    {imgSrc: 'assets/images/pinkfloyd.jpeg', name: 'pink floyd'},
+    {imgSrc: 'assets/images/beatles.jpeg', name: 'beatles'},
+    {imgSrc: 'assets/images/blink182.jpeg', name: 'blink 182'},
+    {imgSrc: 'assets/images/fkatwigs.jpeg', name: 'fka twigs'},
+    {imgSrc: 'assets/images/fleetwood.jpeg', name: 'fleetwood'},
+    {imgSrc: 'assets/images/joy-division.jpeg', name: 'joy division'},
+    {imgSrc: 'assets/images/ledzep.jpeg', name: 'led zeppelin'},
+    {imgSrc: 'assets/images/metallica.jpeg', name: 'metallica'},
+    {imgSrc: 'assets/images/pinkfloyd.jpeg', name: 'pink floyd'}
+];
 
-    this.classList.toggle('flip');
-
-    if(!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        secondCard = this;
-
-        checkForMatch();
-    }
+// randomize cards
+const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
 }
 
-function checkForMatch() {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();
-    } else {
-        unflipCards();
-    }
-}
-
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-    resetBoard();
-}
-
-function unflipCards() {
-lockBoard = true;
-
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-
-        resetBoard();
-    }, 1500)
-}
-
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = null;
-}
-
-(function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
-        card.style.order = randomPos;
+// Card generator function
+const cardGenerator = () => {
+    const cardData = randomize();
+    // generate HTML
+    cardData.forEach(item => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = 'card';
+        face.classList = 'face';
+        back.classList = 'back';
+        // attach info to cards
+        face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
+        // attach cards to section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+        card.addEventListener('click', (e) => {
+            card.classList.toggle('toggleCard');
+            checkCards(e);
+        });
     });
-})();
+};
 
+// check cards
+const checkCards = (e) => {
+    console.log(e);
+    const clickedCard = e.target
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped');
+    console.log(flippedCards);
+    // Logic
+    if (flippedCards.length === 2){
+        if(
+            flippedCards[0].getAttribute('name') ===
+            flippedCards[1].getAttribute('name')
+        ) {
+            console.log('match');
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped');
+                card.style.pointerEvents = 'none';
+            })
+        } else {
+            console.log('wrong');
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped');
+                setTimeout(() => {
+                    card.classList.remove('toggleCard')
+                }, 1000)
+            })
+        }
+    }
+};
 
-cards.forEach(card => {
-    card.addEventListener('click', flipCard)
-});
+cardGenerator();
